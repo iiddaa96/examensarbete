950e8f04b52474bd33d327f09bbe0df899ba0ec8
Ida Casperson <ida_casperson@hotmail.com>
2024-11-18 17:08:35 +0100

    added header and subHeader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import ResponsiveAppBar from "./ui/ResponsiveAppBar";
 
 interface LayoutProps {
   children: ReactNode;
@@ -7,7 +8,10 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body style={{ margin: "0" }}>{children}</body>
+      <body style={{ margin: "0" }}>
+        <ResponsiveAppBar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/ui/Header.tsx
@@ -0,0 +1,141 @@
+// components/Header.tsx
+"use client";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Button } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+import * as React from "react";
+
+interface HeaderProps {
+  handleOpenNavMenu: (event: React.MouseEvent<HTMLElement>) => void;
+  anchorElNav: null | HTMLElement;
+  handleCloseNavMenu: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({
+  handleOpenNavMenu,
+  anchorElNav,
+  handleCloseNavMenu,
+}) => {
+  return (
+    <AppBar
+      position="static"
+      component="header"
+      sx={{
+        backgroundColor: "#ffffff",
+        borderBottom: "1px solid black",
+        boxShadow: "none",
+        color: "black",
+      }}
+    >
+      <Box
+        component={Link}
+        href="/"
+        sx={{
+          display: { xs: "none", md: "inline-block" },
+          overflow: "hidden",
+          marginRight: "30rem",
+        }}
+      >
+        LUXE
+      </Box>
+
+      {/* Mobil Menu Knapp */}
+      <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleOpenNavMenu}
+          color="inherit"
+        >
+          <MenuIcon />
+        </IconButton>
+        {/* Mobil Logo */}
+        <Box component={Link} href="/" sx={{ textDecoration: "none" }}>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "black",
+              textDecoration: "none",
+              padding: "10px",
+            }}
+          >
+            Hejsan
+          </Typography>
+        </Box>
+      </Box>
+      {/* Mobil meny */}
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorElNav}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseNavMenu}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "100%",
+            maxWidth: "1200px",
+            flexWrap: "wrap",
+          }}
+        >
+          <Button
+            component={Link}
+            href="/admin"
+            color="inherit"
+            sx={{ padding: "10px" }}
+          >
+            Admin
+          </Button>
+          <Button
+            component={Link}
+            href="/about us"
+            color="inherit"
+            sx={{ padding: "10px" }}
+          >
+            About us
+          </Button>
+          <Button
+            component={Link}
+            href="/favorite"
+            color="inherit"
+            sx={{ padding: "10px" }}
+          >
+            Favorite
+          </Button>
+          <Button
+            component={Link}
+            href="/contact"
+            color="inherit"
+            sx={{ padding: "10px" }}
+          >
+            Contact
+          </Button>
+        </Box>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Header;
